146f9547d84d4336ff16ca747f0914497f90c23c
HenryKlinck1 <hklinck@cps.edu>
2024-12-18 10:58:46 -0600

    wed

--- part4-classification/a6_part4.py
+++ part4-classification/a6_part4.py
@@ -10,18 +10,43 @@ x = data[["Age", "EstimatedSalary", "Gender"]].values
 y = data["Purchased"].values
 
 # Step 1: Print the values for x and y
+print(x)
+print(y)
 
-# Step 2: Standardize the data using StandardScaler, 
+# Step 2: Standardize the data using StandardScaler
+scaler = StandardScaler().fit(x)
 
 # Step 3: Transform the data
+x = scaler.transform(x)
 
 # Step 4: Split the data into training and testing data
+x_train, x_test, y_train, y_test = train_test_split(x, y)
 
-# Step 5: Fit the data
+# Step 5: Create a LogisticRegression object and fit the data
+model = linear_model.LogisticRegression()
 
-# Step 6: Create a LogsiticRegression object and fit the data
+# Step 6: Fit the model with training data
+model.fit(x_train, y_train)
 
 # Step 7: Print the score to see the accuracy of the model
+print("Accuracy:", model.score(x_test, y_test))
 
 # Step 8: Print out the actual ytest values and predicted y values
 # based on the xtest data
+print("*************")
+print("Testing Results:")
+print("")
+
+label_mapping = {0: "Not Purchased", 1: "Purchased"}
+
+for index in range(len(x_test)):
+    x_val = x_test[index]
+    x_val = x_val.reshape(-1, 3) 
+    y_pred = int(model.predict(x_val))
+
+    actual = y_test[index]
+    predicted_label = label_mapping[y_pred]
+    actual_label = label_mapping[actual]
+
+    print(f"Prediction: {predicted_label}, Actual: {actual_label}")
+    print("")
